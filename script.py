55892286a98eceee02506bcc2dc2a3406df3352f
Slava <inaffectiondbm@gmail.com>
2026-06-23 16:44:47 +0200

    finished the project

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import cv2
 import mediapipe as mp
+import math
 
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(max_num_hands=1)
@@ -19,14 +20,29 @@ while True:
     if result.multi_hand_landmarks == None:
         continue
     landmarks = result.multi_hand_landmarks[0].landmark
-    tip = landmarks[8]
-    x = int(tip.x * frame.shape[1])
-    y = int(tip.y * frame.shape[0])
-    x_y = (x, y)
-    cv2.rectangle(frame, (x-35, y-35), (x+35, y+35), (0, 255, 0), 3)
-    trail.append((x, y))
-    for point in trail:
-        cv2.circle(frame, point, 5, (0, 0, 255), -1)
+    tip_index = landmarks[8]
+    tip_thumb = landmarks[4]
+
+    x_thumb = int(tip_thumb.x * frame.shape[1])
+    y_thumb = int(tip_thumb.y * frame.shape[0])
+
+    x_index = int(tip_index.x * frame.shape[1])
+    y_index = int(tip_index.y * frame.shape[0])
+
+    x_y = (x_index, y_index)
+    cv2.rectangle(frame, (x_index-35, y_index-35), (x_index+35, y_index+35), (0, 255, 0), 3)
+    
+    if len(trail) > 200:
+        trail.pop(0)
+    
+    distance = math.sqrt((x_thumb-x_index)**2 + (y_thumb-y_index)**2)
+    if distance > 30:
+        trail.append((x_index, y_index))
+        for i in range(1, len(trail)):
+            cv2.line(frame, trail[i-1], trail[i], (0, 0, 255), 3)
+    else:
+        trail.clear()
+
     cv2.imshow("name", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
